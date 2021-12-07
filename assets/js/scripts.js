f9c2b9b30c8f9bcd66fd4a8398e8ca849c75ef36
Hipjea <pierre.duverneix@gmail.com>
2021-12-07 10:42:53 +0100

    js

---
+++ assets/js/scripts.js
@@ -0,0 +1,113 @@
+/*
+   
+    Template Name : WebRes - Personal Resume Template
+    Author : UiPasta Team
+    Website : http://www.uipasta.com/
+    Support : http://www.uipasta.com/support/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click
+   5. Scroll To Top
+   6. Tooltip
+   8. Portfolio Filtering
+ 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).load(function () {
+            $('.preloader').delay(800).fadeOut('slow');
+        });
+		
+		
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Scroll Naviagation Background Change with Sticky Navigation */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 70) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+		
+		
+		
+		
+       /* Mobile Navigation Hide or Collapse on Click */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		 
+		
+		
+		
+        /* Scroll To Top */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+	   });
+	
+	
+	    $('.scroll-to-top').click(function(){
+		  $('html, body').animate({scrollTop : 0},800);
+		  return false;
+	    });
+		
+
+        
+        /* Tooltip */
+         
+         $(function () {
+           $('[data-toggle="tooltip"]').tooltip()
+           })
+		   
+		
+    
+            });
+
+   })(jQuery);
